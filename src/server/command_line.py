2ed720a813c95bbef75a4f732f770c14fea900c6
LeshLee33 <al.sahnoff@yandex.ru>
2024-12-06 23:05:21 +0700

    Испровление docker-compose.yml (не копировал конфигурационный файл)

---
+++ src/server/command_line.py
@@ -0,0 +1,58 @@
+import click
+from router import database
+from router.database_connection import users_collection
+
+
+@click.group()
+def cli():
+    """Управление пользователями."""
+    print("Система управления пользователями запущена.")
+
+
+@cli.command()
+@click.argument('nickname')
+@click.argument('password')
+def create_user(nickname, password):
+    """Создание нового пользователя."""
+    new_user = dict(nickname=nickname, password=password, bookmarks=[], books_own=[])
+
+    user = users_collection.find_one({"nickname": nickname})
+    if user:
+        print("Пользователь с таким псевдонимом уже существует")
+        return
+    users_collection.insert_one(new_user)
+    click.echo(f'Пользователь "{nickname}" успешно создан!')
+
+
+@cli.command()
+@click.argument('nickname')
+def read_user_info(nickname):
+    """Просмотр пользователя по псевдониму."""
+    query = dict(nickname=nickname)
+    user = users_collection.find_one(query)
+
+    if user is None:
+        print("Такого пользователя не существует")
+    else:
+        print(f"Псевдоним: {user['nickname']}, "
+              f"пароль: {user['password']}, "
+              f"закладки: {user['bookmarks']}, "
+              f"авторство: {user['books_own']}")
+
+
+@cli.command()
+@click.argument('nickname')
+def delete_user(nickname):
+    """Удаление пользователя."""
+    query = dict(nickname=nickname)
+    user = users_collection.find_one(query)
+
+    if user is None:
+        print("Такого пользователя не существует")
+    else:
+        users_collection.insert_one(query)
+    click.echo(f'Пользователь "{nickname}" успешно удалён!')
+
+
+if __name__ == '__main__':
+    cli()
